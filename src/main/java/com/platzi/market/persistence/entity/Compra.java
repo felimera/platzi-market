afadf7d9f3c03a957f51c11efdbc974107f02e9d
andres.mera <andres.mera@sitis.com.co>
2022-03-10 11:48:31 -0500

    Crear el repository de compras.

--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -21,7 +21,7 @@ public class Compra {
     @ManyToOne
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
-    @OneToMany(mappedBy = "producto")
+    @OneToMany(mappedBy = "compra", cascade = {CascadeType.ALL})
     private List<CompraProducto> productos;
 
     public Integer getIdCompra() {
