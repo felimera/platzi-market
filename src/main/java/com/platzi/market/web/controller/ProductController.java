6813942e88177c3edb26784bb14672d648f36ede
andres.mera <andres.mera@sitis.com.co>
2022-03-10 08:56:50 -0500

    Exponer nuestra API.

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -5,8 +5,7 @@ import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,23 +16,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/all")
     public ResponseEntity<List<Product>> getAll() {
         return ResponseEntity.ok(productService.getAll());
     }
 
-    public ResponseEntity<Optional<Product>> getProduct(int productId) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Product>> getProduct(@PathVariable("id") int productId) {
         return ResponseEntity.ok(productService.getProduct(productId));
     }
 
-    public ResponseEntity<Optional<List<Product>>> getByCategory(int categoryId) {
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<Optional<List<Product>>> getByCategory(@PathVariable("categoryId") int categoryId) {
         return ResponseEntity.ok(productService.getByCategory(categoryId));
     }
 
-    public ResponseEntity<Product> save(Product product) {
+    @PostMapping("/save")
+    public ResponseEntity<Product> save(@RequestBody Product product) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
-    public ResponseEntity<Boolean> delete(int productId) {
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") int productId) {
         return ResponseEntity.ok(productService.delete(productId));
     }
 }
