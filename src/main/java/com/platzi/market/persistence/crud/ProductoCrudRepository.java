70a5583d5561ca784d07a76bf9f549a07d19f7cd
andres.mera <andres.mera@sitis.com.co>
2022-03-10 08:19:32 -0500

    Inyeccion de dependencias.

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -2,12 +2,10 @@ package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
 
